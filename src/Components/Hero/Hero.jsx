f28af9778d92bc557c2afe840d153e3f861e5d89
shreya <shreyacic123@gmail.com>
2024-01-03 14:44:37 +0530

    changes1

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
   return (
     <div className='hero'>
    <div className="hero-left">
-    <h2>ARRIVALS ONLY</h2>
+    <h2>NEW ARRIVALS ONLY</h2>
     <div>
       <div className="hero-hand-icon">
         <p>new</p>
